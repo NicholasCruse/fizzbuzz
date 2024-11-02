04b8252f2c74b4394f9e4d148eae9d00a3109b33
NicholasCruse <ncruselex@gmail.com>
2024-11-01 22:46:46 -0400

    Nick Cruse working FizzBuzz code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
-# add your code here
-
+#loops from 1 to 100
+for fizzynum in range(1, 101):
+    #checks if the number is divisible by 3 and 5
+    if fizzynum % 3 == 0 and fizzynum % 5 == 0:
+        print("fizzbuzz")
+    #checks if the number is divisible by only 3
+    elif fizzynum % 3 == 0:
+        print("fizz")
+    #checks if number is divisible by only 5
+    elif fizzynum % 5 == 0:
+        print("buzz")
+    #it prints the number when its neither divisible by 3 nor 5
+    else:
+        print(fizzynum)
